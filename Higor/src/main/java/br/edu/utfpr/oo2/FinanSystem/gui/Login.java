7fa694c4aa681464e7f6eb70674f442c542ab3a2
Higor Freitas <46633539+HigorFreitaz@users.noreply.github.com>
2025-11-13 15:34:33 -0300

    Login
    Criação de tela de login

--- Higor/src/main/java/br/edu/utfpr/oo2/FinanSystem/gui/Login.java
+++ Higor/src/main/java/br/edu/utfpr/oo2/FinanSystem/gui/Login.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class main extends JFrame {
+public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -18,7 +18,7 @@ public class main extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					main frame = new main();
+					Login frame = new Login();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,7 +30,7 @@ public class main extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public main() {
+	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
